f639f5b9d47d1220657a44dc98b5450af87d955d
FelixE <erotufelixpascal2@gmail.com>
2024-03-08 15:25:09 +0300

    added routes for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomePageComponent } from "./LoanOfficer/home-page/home-page.component";
+import { RegisterLoanComponent } from "./LoanOfficer/register-loan/register-loan.component";
+import { ViewLoanComponent } from "./LoanOfficer/view-loan/view-loan.component";
+import { ClientRecordComponent } from "./LoanOfficer/client-record/client-record.component";
 
 
 export const routes: Routes = [ 
-    { path:"home-page" , component: HomePageComponent}
+    { path:"loan-officer/home-page" , component: HomePageComponent},
+    { path:"loan-officer/register-loan" , component: RegisterLoanComponent},
+    { path:"loan-officer/view-loan" , component: ViewLoanComponent},
+    { path:"loan-officer/client-record" , component: ClientRecordComponent}
 ];
 
 @NgModule({
